5cc094e7e437c1df5cf9451e93ec815e14d7b616
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-04-19 23:09:49 +0530

    Time: 174 ms (62.54%) | Memory: 94.1 MB (85.89%) - LeetSync

--- longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int m = 0;
@@ -6,7 +17,7 @@ public:
         if (root == NULL){return 0;}
         int a = explore(root->left,1);
         int b = explore(root->right,0);
-        m = max(m,max(a+1,b+1));
+        m = max(m,max(a,b));
         if (turn == 0){return 1+a;}
         if (turn == 1){return 1+b;}
         return -1;
@@ -15,7 +26,6 @@ public:
     int longestZigZag(TreeNode* root) {
         int x = explore(root->left,1);
         int y = explore(root->right,0);
-        cout << x << " " << y << " " << m << endl;
-        return max(m,max(x+1,y+1)) - 1;
+        return max(m,max(x,y));
     }
 };
