21d9ecb97732c3c1f2f9fb2bf99dd0337efa83f7
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-01 03:30:15 +0530

    Time: 67 ms (91.07%) | Memory: 9.9 MB (99.04%) - LeetSync

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int *dp;
-    int recur(vector<int>&coins, int amount)
-    {
-        if (amount == 0){return 0;}
-        if (amount<0){return INT_MAX;}
-        if (dp[amount] != -1){return dp[amount];}
-        int m = INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {
-            m = min(recur(coins,amount - coins[i]),m);
-        }
-        if (m!=INT_MAX){m++;}
-        dp[amount] = m;
-        return m;
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        dp = new int[amount+1];
-        for(int i=0;i<amount+1;i++)
+        int dp[amount+1];
+        dp[0] = 0;
+        for(int i=1;i<amount+1;i++)
         {
-            dp[i] = -1;
+            int m = INT_MAX;
+            for(int j=0;j<coins.size();j++)
+            {
+                if (i-coins[j]>=0)
+                {
+                    m = min(dp[i-coins[j]],m);
+                }
+            }
+            if (m!=INT_MAX){m++;}
+            dp[i] = m;
         }
-        int ans =  recur(coins,amount);
-        delete [] dp;
-        if (ans == INT_MAX){return -1;}
-        return ans;
+        if (dp[amount] == INT_MAX){return -1;}
+        return dp[amount];
     }
 };
