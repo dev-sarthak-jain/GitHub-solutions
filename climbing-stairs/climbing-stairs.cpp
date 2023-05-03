8153a892419f46ee14c4944596aa8c3097c766eb
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-03 12:28:14 +0530

    Time: 3 ms (38.53%) | Memory: 5.9 MB (96.27%) - LeetSync

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         if (n<2){return n;}
-        int dp[n];
-        dp[0] = 1;
-        dp[1] = 2;
+        int a = 1,b = 2;
+        int c;
         for(int i=2;i<n;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a+b;
+            a = b;
+            b=c;
         }
-        return dp[n-1];
+        return c;
     }
 };
