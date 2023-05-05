ee85eed8d670ba3ffd52da21232ce2a8d9e52d80
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-05 15:49:33 +0530

    Time: 24 ms (57.53%) | Memory: 19.5 MB (46.53%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        vector<bool> used(10, false);
-        vector<int> row(9), col(9), sec(9);
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] != '.') {
-                    int num = board[i][j] - '0';
-                    int secIdx = i/3 * 3 + j/3;
-                    if(used[num] || row[i] & (1<<num) || col[j] & (1<<num) || sec[secIdx] & (1<<num)) {
-                        return false;
-                    }
-                    used[num] = true;
-                    row[i] |= (1<<num);
-                    col[j] |= (1<<num);
-                    sec[secIdx] |= (1<<num);
-                }
+    bool isValidSudoku(vector<vector<char>>& b) {
+        vector<bool> temp(10,0);
+        vector<vector<bool>> row(9,temp);
+        vector<vector<bool>> col(9,temp);
+        vector<vector<bool>> sect(9,temp);
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if (b[i][j] == '.') { continue;}
+                int x = b[i][j] - 48;
+                if (row[i][x]){return 0;}
+                else {row[i][x] = 1;}
+                if (col[j][x]){return 0;}
+                else {col[j][x] = 1;}
+                if (sect[((i/3)*3 + j/3)][x]){return 0;}
+                else {sect[((i/3)*3 + j/3)][x] = 1;}
             }
-            used.assign(10, false);
         }
-        return true;
+        return 1;
     }
 };
