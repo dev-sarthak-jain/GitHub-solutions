8db4d9587e5b00bebb24302d1ac66e37d3ab17f1
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-05 15:48:59 +0530

    Time: 32 ms (22.35%) | Memory: 18 MB (72.41%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& b) {
-        vector<bool> temp(10, false);
-        vector<vector<bool>> row(9, temp);
-        vector<vector<bool>> col(9, temp);
-        vector<vector<bool>> sect(9, temp);
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (b[i][j] == '.') continue;
-                int x = b[i][j] - '0';
-                if (row[i][x] || col[j][x] || sect[(i / 3) * 3 + j / 3][x]) return false;
-                row[i][x] ? false : row[i][x] = true;
-                col[j][x] ? false : col[j][x] = true;
-                sect[(i / 3) * 3 + j / 3][x] ? false : sect[(i / 3) * 3 + j / 3][x] = true;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<bool> used(10, false);
+        vector<int> row(9), col(9), sec(9);
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    int secIdx = i/3 * 3 + j/3;
+                    if(used[num] || row[i] & (1<<num) || col[j] & (1<<num) || sec[secIdx] & (1<<num)) {
+                        return false;
+                    }
+                    used[num] = true;
+                    row[i] |= (1<<num);
+                    col[j] |= (1<<num);
+                    sec[secIdx] |= (1<<num);
+                }
             }
+            used.assign(10, false);
         }
         return true;
     }
