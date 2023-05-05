9ccf8c8bb008b19cb77cff523c5697ba5462adb1
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-05 14:42:41 +0530

    Time: 45 ms (18.99%) | Memory: 10.1 MB (29.12%) - LeetSync

--- maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
+        unordered_set<char> check({'a','e','i','o','u'});
         int i=0,j=0;
         int m = 0;
         while(j<k)
         {
-            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
-            {
-                m++;
-            }
+            if (check.find(s[j]) != check.end()){m++;}
             j++;
         }
         int count = m;
         while(j<s.length())
         {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-            {
-                count--;
-            }
-            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
-            {
-                count++;
-            }
+            if (check.find(s[i]) != check.end()){count--;}
+            if (check.find(s[j]) != check.end()){count++;}
             m = max(count,m);
-            if (m==k){return m;}
+            //if (m==k){return m;}
             i++;j++;
         }
         return m;
