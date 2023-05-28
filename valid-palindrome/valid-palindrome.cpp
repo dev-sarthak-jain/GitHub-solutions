77f31ec0454850527e849903af107e065cc79a05
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-05-28 19:42:18 +0530

    Time: 7 ms (48.53%) | Memory: 7.4 MB (59.11%) - LeetSync

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i=0,j = s.size()-1;
-        while(i<j)
-        {
-            if (iswalnum(s[i]) == 0){i++;}
-            else if(iswalnum(s[j]) == 0){j--;}
-            else
-            {
-                if (tolower(s[i]) != tolower(s[j])){return 0;}
-                i++;j--;
+        int i = 0;
+        int j = s.size() - 1;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);  // Convert string to lowercase
+
+        while (i < j) {
+            if (!iswalnum(s[i])) {
+                i++;
+            } else if (!iswalnum(s[j])) {
+                j--;
+            } else {
+                if (s[i] != s[j]) {
+                    return false;
+                }
+                i++;
+                j--;
             }
         }
-        return 1;
+
+        return true;
     }
 };
