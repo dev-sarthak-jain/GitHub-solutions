51f4faa2260719b7a03cff4303fdf722814394eb
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-06-05 11:59:38 +0530

    Time: 9 ms (63.43%) | Memory: 10 MB (63.27%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int n;
-    double m;
-    int x,y;
-    bool recur(vector<vector<int>>& nums,int a)
-    {
-        if (a == n){return 1;}
-        int denom = x-nums[a][0];
-        if (denom == 0){return 0;}
-        if (m == (double(y-nums[a][1]))/denom){return recur(nums,a+1);}
-        else{return 0;}
-
-    }
-    bool denominator(vector<vector<int>>& nums,int a)
-    {
-        if (a == n){return 1;}
-        int denom = x-nums[a][0];
-        if (denom == 0){return denominator(nums,a+1);}
-        else{return 0;}
-    }
-
     bool checkStraightLine(vector<vector<int>>& nums) {
+        double m;
+        int n,x,y;
         n = nums.size();
         x = nums[0][0];
         y = nums[0][1];
         int denom = nums[1][0]-x;
-        if (denom == 0){return denominator(nums,2);}
-        m = (double(nums[1][1]-y))/(nums[1][0]-x);
-        return recur(nums,2);
+        if (denom == 0)
+        {
+            for(int i=2;i<n;i++)
+            {
+                if (x - nums[i][0] != 0)
+                {return 0;}
+            }
+            return 1;
+        }
+        else
+        {
+            m = (double(nums[1][1]-y))/denom;
+            for(int i=2;i<n;i++)
+            {
+                if (m != (double(nums[i][1]-y))/(nums[i][0]-x))
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
     }
 };
