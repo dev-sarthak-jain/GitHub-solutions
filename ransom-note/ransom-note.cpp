2671d79a91ebf99098f47fe80edb68c67ff3337c
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-06-17 18:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (99.26%) - LeetSync

--- ransom-note/ransom-note.cpp
+++ ransom-note/ransom-note.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    bool canConstruct(const string& ransomNote, const string& magazine) {
-        unordered_map<char, int> map;
-        
-        for (const char c : magazine) {
-            map[c]++;
+    bool canConstruct(const string& ransom, const string& magazine) {
+        vector<int> arr(26,0);
+        for (int i=0;i<magazine.length();i++)
+        {
+            ++arr[magazine[i]-97];
         }
-        
-        for (const char c : ransomNote) {
-            auto it = map.find(c);
-            if (it == map.end() || it->second <= 0) {
-                return false;
-            }
-            it->second--;
+        for(int i=0;i<ransom.length();i++)
+        {
+            if (--arr[ransom[i]-97]<0){return 0;}
         }
         
         return true;
