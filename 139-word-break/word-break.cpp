db57f251865af61fb578c1d7c9e657b3cb786782
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-08-05 17:05:09 +0530

    Time: 25 ms (28.05%) | Memory: 13.8 MB (31.07%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-     unordered_map<string,bool>dp;
-    bool solve(string s,unordered_set<string>&m){
-        if(s.length()==0){
+    unordered_set<string> sett;
+    int n;
+
+    bool recur(string s, int x, vector<int>& check) {
+        if (n == x) {
             return true;
         }
-        if(dp.find(s)!=dp.end())
-        return dp[s];
-        for(int i=0;i<s.length();i++){
-            string r=s.substr(0,i+1);
-            if(m.count(r)){
-                if(solve(s.substr(i+1),m))
-                return dp[s]=true;
+        cout << x << endl;
+        for (int i = x; i < n; i++) {
+            if (sett.find(s.substr(x, i - x + 1)) != sett.end()) {
+                if (check[i + 1] == 1) {
+                    check[x] = true;
+                    return true;
+                }
+                else if (check[i+1] == -1 && recur(s, i + 1, check))
+                {
+                    check[x] = true;
+                    return true;
+                }
             }
         }
-        return dp[s]=false;
+        check[x] = false;
+        return false;
     }
-    bool wordBreak(string s, vector<string>& wordDict) {
-         unordered_set<string>m;
-        for(auto x:wordDict){
-            m.insert(x);
+
+    bool wordBreak(string s, vector<string>& word) {
+        n = s.length();
+        for (int i = 0; i < word.size(); i++) {
+            sett.insert(word[i]);
         }
-         return solve(s,m);
+        vector<int> check(n + 1, -1);
+        return recur(s, 0, check);
     }
 };
