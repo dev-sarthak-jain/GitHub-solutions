84c0b92cc41217e65b5817dcd188ffd4e9226567
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-08-10 01:46:53 +0530

    Time: 2 ms (75.08%) | Memory: 11 MB (50.91%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,34 @@
+#define rf return recur(nums,mid+1,y)
+#define lf return recur(nums,x,mid-1)
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int i=0;
-        int j=nums.size()-1;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>=nums[i]){
-                if(target>=nums[i]&& target<nums[mid]){
-                    j=mid-1;
-                }
-                else
-                    i=mid+1;
-            }
-            else if(nums[mid]<=nums[j]){
-                if(target>nums[mid]&&target<=nums[j])
-                    i=mid+1;
-                else
-                j=mid-1;
+    int target;
+    int recur(vector<int>& nums, int x, int y)
+    {
+        if (x>y){return -1;}
+
+        int mid = x + (y-x)/2;
+        if (nums[mid] == target){return mid;}
+        else if (nums[mid]>=nums[x])
+        {
+            if (nums[mid]<target){rf;}
+            else{
+                if (target<nums[x]){rf;}
+                else{lf;}
             }
         }
-        return -1;
+        else
+        {
+            if (target<nums[mid]){lf;}
+            else{
+                if (target<=nums[y]){rf;}
+                else{lf;}
+            }
+        }
+    }
+
+    int search(vector<int>& nums, int t) {
+        target = t;
+        return recur(nums,0,nums.size()-1);
     }
 };
