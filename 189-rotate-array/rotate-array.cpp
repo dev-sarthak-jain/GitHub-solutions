96213ffb65949c7dffa4df33ad2bc44c0b01870c
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2023-11-20 12:30:23 +0530

    Time: 20 ms (74.50%) | Memory: 25.1 MB (98.66%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,23 +1,11 @@
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size(),j,z;
-        k = k%n;
-        vector<bool> check(n,0);
-        for(int i=0; i<k; i++)
-        {
-            j = i;
-            int temp1 = nums[j];
-            int temp2;
-            while(check[j]==0)
-            {
-                check[j] = 1;
-                z = (j+k)%n;
-                temp2 = nums[z];
-                nums[z] = temp1;
-                temp1 = temp2;
-                j = z;
-            }
-        }
+    void rotate(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
