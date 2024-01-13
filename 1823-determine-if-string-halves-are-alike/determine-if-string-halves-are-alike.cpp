712c0a85c7b605b8e0dfced1d293cd5436625a3d
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-01-13 09:29:59 +0530

    Time: 4 ms (44.44%) | Memory: 7 MB (46.72%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i=0;i<n/2;i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                sum++;
+            }
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                sum--;
+            }
+        }
+        if (sum==0){return true;}
+        else{return false;}
+    }
+};
