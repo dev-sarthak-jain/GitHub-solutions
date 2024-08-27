dd92352965111ecf7c0202da8f8ce5b79d2feefc
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-27 09:59:33 +0530

    Time: 71 ms (64.61%) | Memory: 26.2 MB (55.42%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> alpha(26,0);
+        vector<int> alpha(26, 0);
         int n = word.size();
-        for(int i=0;i<n;i++)
-        {
-            alpha[word[i]-'a']++;
-        }
-        sort(alpha.begin(),alpha.end(),greater<int>());
-        int ans = 0;
-        int res = 1;
-        int i=0;
-        while(i<26 && alpha[i]!=0)
-        {
-            for (int j=0;i<26 && j<8;j++)
-            {
-                ans += res*alpha[i];
-                i++;
-            }
-            res++;
-        }
-        return ans;
 
+        // Count frequency of each character
+        for (int i = 0; i < n; i++) {
+            alpha[word[i] - 'a']++;
+        }
+
+        // Use a max-heap to keep track of the highest frequencies
+        priority_queue<int> maxHeap;
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] > 0) {
+                maxHeap.push(alpha[i]);
+            }
+        }
+
+        int ans = 0;
+        int multiplier = 1;
+
+        // Calculate the minimum pushes
+        while (!maxHeap.empty()) {
+            for (int i = 0; i < 8 && !maxHeap.empty(); i++) {
+                int freq = maxHeap.top();
+                maxHeap.pop();
+                ans += freq * multiplier;
+            }
+            multiplier++;
+        }
+
+        return ans;
     }
 };
