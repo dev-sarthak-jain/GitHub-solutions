1304f6f766eaf6b95c63987bc9a0c49ad5c6f15a
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-29 11:54:21 +0530

    Time: 7 ms (45.02%) | Memory: 9.3 MB (95.25%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    bool valid(vector<string>&check, int x, int y, int n)
+    {
+        for (int i=x-1;i>=0;i--)
+        {
+            if (check[i][y] == 'Q'){return 0;}
+        }
+        int a = x-1, b= y-1;
+        while(a>=0 && b>=0)
+        {
+            if (check[a][b] == 'Q'){return 0;}
+            a--;b--;
+        }
+        a = x-1;
+        b = y+1;
+        while (a>=0 && b<n)
+        {
+            if (check[a][b] == 'Q'){return 0;}
+            a--;b++;
+        }
+        return 1;
+    }
+    
+    void func(vector<vector<string>>& answer, vector<string>& check, int n, int z)
+    {
+        if (n==z){answer.push_back(check);}
+
+
+        for (int i=0; i<n;i++)
+        {
+            if (valid(check,z,i,n))
+            {
+                check[z][i] = 'Q';
+                func(answer,check,n,z+1);
+                check[z][i] = '.';
+            }
+        }
+        return;
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        string s = "";
+        for(int i=0;i<n;i++)
+        {
+            s+= ".";
+        }
+        vector<string> check(n,s);
+        vector<vector<string>> ans;
+        func(ans,check,n,0);
+        return ans;
+    }
+};
