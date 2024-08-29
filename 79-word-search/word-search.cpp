c51a2c4463de2d649f07e7ee718b3e7f24ca6c7b
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-29 16:09:39 +0530

    Time: 1265 ms (9.35%) | Memory: 12.3 MB (9.47%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    int n,m,wsize;
     bool valid(int x,int y, int n, int m)
     {
         if (x>=0 && x<n && y>=0 && y<m){return true;}
         else{return false;}
     }
 
-    bool func(vector<vector<char>>& board,vector<vector<bool>>& check, string word, int x, int y, int c, int n, int m)
+    bool func(vector<vector<char>>& board,vector<vector<bool>>& check, string word, int x, int y, int c)
     {
-        if (c==word.length()){return true;}
+        if (c==wsize){return true;}
         if (valid(x,y,n,m) && !check[x][y] && word[c] == board[x][y])
         {
             check[x][y] = 1;
             if (
-            func(board,check,word,x+1,y,c+1,n,m) ||
-            func(board,check,word,x-1,y,c+1,n,m) ||
-            func(board,check,word,x,y+1,c+1,n,m) ||
-            func(board,check,word,x,y-1,c+1,n,m))
+            func(board,check,word,x+1,y,c+1) ||
+            func(board,check,word,x-1,y,c+1) ||
+            func(board,check,word,x,y+1,c+1) ||
+            func(board,check,word,x,y-1,c+1))
             {return 1;}
             check[x][y] = 0;
         }
@@ -25,9 +26,9 @@ public:
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        int c = word.size();
+        n = board.size();
+        m = board[0].size();
+        wsize = word.length();
         vector<vector<bool>> check(n,vector<bool>(m,0));
         for(int i=0;i<n;i++)
         {
@@ -37,10 +38,10 @@ public:
                 {
                     check[i][j] = 1;
                     if (
-                    func(board,check,word,i+1,j,1,n,m) ||
-                    func(board,check,word,i-1,j,1,n,m) ||
-                    func(board,check,word,i,j+1,1,n,m) ||
-                    func(board,check,word,i,j-1,1,n,m))
+                    func(board,check,word,i+1,j,1) ||
+                    func(board,check,word,i-1,j,1) ||
+                    func(board,check,word,i,j+1,1) ||
+                    func(board,check,word,i,j-1,1))
                     {return 1;}
                     check[i][j] = 0;
                 }
