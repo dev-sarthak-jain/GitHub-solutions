db5423fdf42782bf19e3b9ba25dbc1419d88a0ea
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-29 12:36:26 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (35.34%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> temp;
-    int n;
-    void recur(vector<int>& nums,vector<bool>& check, int x)
+    
+    void func(vector<vector<int>>& ans, vector<bool>& check, vector<int>& nums,vector<int>& arr, int n)
     {
-        if (x==n){ans.push_back(temp);}
-
+        if (arr.size() == n){ans.push_back(arr);return;}
         for(int i=0;i<n;i++)
         {
-            if (check[i] != 1)
-            {
-                check[i] = 1;
-                temp.push_back(nums[i]);
-                recur(nums,check,x+1);
-                temp.pop_back();
-                check[i] = 0;
+            int temp = nums[i]+10;
+            if (check[temp]==0){
+                check[temp] = 1;
+                arr.push_back(nums[i]);
+                func(ans,check,nums,arr, n);
+                arr.pop_back();
+                check[temp] = 0;
             }
         }
     }
 
+
     vector<vector<int>> permute(vector<int>& nums) {
-        n = nums.size();
-        vector<bool> check(n,0);
-        recur(nums,check,0);
+        vector<bool> check (21,0);
+        vector<vector<int>> ans;
+        vector<int> arr;
+        func(ans,check,nums,arr,nums.size());
         return ans;
     }
 };
