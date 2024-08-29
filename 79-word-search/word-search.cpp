0d61cc1f908afe7233c968e4602b714c8b5a5493
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-29 16:07:46 +0530

    Time: 1233 ms (10.20%) | Memory: 12 MB (27.36%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
-    // Function to check if a position is within the board's bounds
-    bool valid(int x, int y, int n, int m) {
-        return x >= 0 && x < n && y >= 0 && y < m;
+    bool valid(int x,int y, int n, int m)
+    {
+        if (x>=0 && x<n && y>=0 && y<m){return true;}
+        else{return false;}
     }
 
-    // Recursive function to perform depth-first search
-    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& check, const string& word, int x, int y, int c, int n, int m) {
-        // Check if the current index matches the length of the word
-        if (c == word.length()) return true;
-        
-        // Check bounds and whether the cell has been visited
-        if (!valid(x, y, n, m) || check[x][y] || board[x][y] != word[c]) return false;
+    bool func(vector<vector<char>>& board,vector<vector<bool>>& check, string word, int x, int y, int c, int n, int m)
+    {
+        if (c==word.length()){return true;}
+        if (valid(x,y,n,m) && !check[x][y] && word[c] == board[x][y])
+        {
+            check[x][y] = 1;
+            if (
+            func(board,check,word,x+1,y,c+1,n,m) ||
+            func(board,check,word,x-1,y,c+1,n,m) ||
+            func(board,check,word,x,y+1,c+1,n,m) ||
+            func(board,check,word,x,y-1,c+1,n,m))
+            {return 1;}
+            check[x][y] = 0;
+        }
+        return false;
 
-        // Mark the cell as visited
-        check[x][y] = true;
-
-        // Explore all four directions
-        bool found = dfs(board, check, word, x + 1, y, c + 1, n, m) ||
-                     dfs(board, check, word, x - 1, y, c + 1, n, m) ||
-                     dfs(board, check, word, x, y + 1, c + 1, n, m) ||
-                     dfs(board, check, word, x, y - 1, c + 1, n, m);
-
-        // Unmark the cell to allow other paths to use it
-        check[x][y] = false;
-
-        return found;
     }
 
-    // Main function to start the search
-    bool exist(vector<vector<char>>& board, const string& word) {
+    bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<bool>> check(n, vector<bool>(m, false));
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                // Start the DFS if the first character matches
-                if (board[i][j] == word[0] && dfs(board, check, word, i, j, 0, n, m)) {
-                    return true;
+        int c = word.size();
+        vector<vector<bool>> check(n,vector<bool>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if (board[i][j] == word[0])
+                {
+                    check[i][j] = 1;
+                    if (
+                    func(board,check,word,i+1,j,1,n,m) ||
+                    func(board,check,word,i-1,j,1,n,m) ||
+                    func(board,check,word,i,j+1,1,n,m) ||
+                    func(board,check,word,i,j-1,1,n,m))
+                    {return 1;}
+                    check[i][j] = 0;
                 }
             }
         }
-
-        return false;
+        return 0;
     }
 };
