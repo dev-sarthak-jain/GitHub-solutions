27ef63a4a08ff7f45bca33bba70a6600871000d6
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-08-29 16:04:29 +0530

    Time: 1257 ms (9.43%) | Memory: 12.2 MB (15.70%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    int m,n;
-    bool bound(int i, int j)
+    bool valid(int x,int y, int n, int m)
     {
-        if (i<0 || i>=n || j<0 || j>=m){return 0;}
-        else {return 1;}
+        if (x>=0 && x<n && y>=0 && y<m){return true;}
+        else{return false;}
     }
 
-    bool func(vector<vector<char>>& board, string& word,vector<vector<bool>>& check, int i,int j, int k)
+    bool func(vector<vector<char>>& board,vector<vector<bool>>& check, string word, int x, int y, int c, int n, int m)
     {
-        if (k == word.length()){return 1;}
-        if (bound(i+1,j) && check[i+1][j] == 0 && board[i+1][j] == word[k]){
-            check[i+1][j] = 1;
-            if (func(board,word,check,i+1,j,k+1)){return 1;}
-            check[i+1][j] = 0;
+        if (c==word.length()){return true;}
+        if (valid(x,y,n,m) && !check[x][y])
+        {
+            if (word[c] == board[x][y])
+            {
+                check[x][y] = 1;
+                if (
+                func(board,check,word,x+1,y,c+1,n,m) ||
+                func(board,check,word,x-1,y,c+1,n,m) ||
+                func(board,check,word,x,y+1,c+1,n,m) ||
+                func(board,check,word,x,y-1,c+1,n,m))
+                {return 1;}
+                check[x][y] = 0;
+            }
         }
-        if (bound(i-1,j) && check[i-1][j] == 0 && board[i-1][j] == word[k]){
-            check[i-1][j] = 1;
-            if (func(board,word,check,i-1,j,k+1)){return 1;}
-            check[i-1][j] = 0;
-        }
-        if (bound(i,j+1) && check[i][j+1] == 0 && board[i][j+1] == word[k]){
-            check[i][j+1] = 1;
-            if (func(board,word,check,i,j+1,k+1)){return 1;}
-            check[i][j+1] = 0;
-        }
-        if (bound(i,j-1) && check[i][j-1] == 0 && board[i][j-1] == word[k]){
-            check[i][j-1] = 1;
-            if (func(board,word,check,i,j-1,k+1)){return 1;}
-            check[i][j-1] = 0;
-        }
-        return 0;
+        return false;
+
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        n = board.size();
-        m = board[0].size();
-        vector<vector<bool>> check (n,vector<bool>(m,0));
+        int n = board.size();
+        int m = board[0].size();
+        int c = word.size();
+        vector<vector<bool>> check(n,vector<bool>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -44,7 +39,12 @@ public:
                 if (board[i][j] == word[0])
                 {
                     check[i][j] = 1;
-                    if (func(board,word,check,i,j,1) == 1){return 1;}
+                    if (
+                    func(board,check,word,i+1,j,1,n,m) ||
+                    func(board,check,word,i-1,j,1,n,m) ||
+                    func(board,check,word,i,j+1,1,n,m) ||
+                    func(board,check,word,i,j-1,1,n,m))
+                    {return 1;}
                     check[i][j] = 0;
                 }
             }
