a6be506fd5b7af2c8716726512be6a66dc216471
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-03 02:14:36 +0530

    Time: 43 ms (43.58%) | Memory: 17.4 MB (56.31%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -9,6 +9,7 @@ public:
         
         for(int i=0;i<n;i++)
         {
+            if (amount - coins[i]<0){break;}
             int res = func(coins,check,amount-coins[i]);
             if (res==-1){continue;}
             ans = min(ans,res);
@@ -22,6 +23,7 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         vector<int> check(amount+1, INT_MAX);
+        sort(coins.begin(),coins.end());
         check[0] = 0;
         n = coins.size();
         int ans = func(coins,check,amount);
