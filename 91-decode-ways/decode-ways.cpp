5bd494dd6a1327aa006179e65464e99ebedbf6e7
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-02 14:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (47.24%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,53 +2,24 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length();
-        int a,b;
-        if (n==1){
-            if (s[0] == '0'){return 0;}
-            return 1;
-        }
-        if (s[0] == '0'){return 0;}
-        else {a=1;}
-
-        if (s[1] == '0' && s[0]>'2'){return 0;}
-        else if (s[1] == '0' && s[0]<'3'){b=1;}
-        else if (s[0]=='1'){b=2;}
-        else if (s[0]=='2' && s[1]<'7'){b=2;}
-        else {b=1;}
-
-        int temp;
-        for(int i=2;i<n;i++)
+        if (n==0 || s[0] == '0'){return 0;}
+        int a =1, b= 1;
+        for(int i=1;i<n;i++)
         {
-            temp = 0;
-            if (s[i] == '0')
+            int temp = 0;
+            if (s[i] != '0')
             {
-                if (s[i-1] == '0' || s[i-1]>'2'){return 0;}
-                else {temp += a;}
+                temp+=b;
             }
-            else if (s[i-1] == '0')
+            int twodigit = stoi(s.substr(i-1,2));
+            if (twodigit>=10 && twodigit<=26)
             {
-                temp = b;
-            }
-            else if (s[i-1]<'3')
-            {
-                if (s[i-1]=='1')
-                {
-                    temp = a+b;
-                }
-                else if (s[i]<'7') 
-                {
-                    temp = a+b;
-                }
-                else{temp = b;}
-            }
-            else
-            {
-                temp =b;
+                temp+=a;
             }
             a = b;
             b = temp;
+            //cout << a << " " << b << endl;
         }
         return b;
-
     }
 };
