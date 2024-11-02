49c426a0d788d1cc93e39d5d46f7f0fc3c4d89de
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-02 15:35:23 +0530

    Time: 1628 ms (5.02%) | Memory: 20.4 MB (47.18%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<long int> vec(amount+1,-1);
+        long int n = coins.size();
+        vec[0] = 0;
+        sort(coins.begin(),coins.end());
+        for(long int i=0;i<=amount;i++)
+        {
+            if (vec[i] == -1){continue;}
+            for(long int j=0;j<n;j++)
+            {
+                if (i+coins[j]<=amount)
+                {
+                    if (vec[i+coins[j]] == -1){vec[i+coins[j]] = vec[i]+1;}
+                    else {vec[i+coins[j]] = min(vec[i+coins[j]],vec[i]+1);}
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<=amount;i++)
+        {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+        return vec[amount];
+    }
+};
