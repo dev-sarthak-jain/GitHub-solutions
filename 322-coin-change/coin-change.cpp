08436b1613f630ca4333b9d666d058815c8e2393
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-03 02:13:18 +0530

    Time: 36 ms (51.39%) | Memory: 17.6 MB (53.63%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<long int> vec(amount+1,-1);
-        long int n = coins.size();
-        vec[0] = 0;
-        sort(coins.begin(),coins.end());
-        for(long int i=0;i<=amount;i++)
+    int n;
+    int func(vector<int>& coins, vector<int>& check, int amount)
+    {
+        if (amount < 0){return INT_MAX;}
+        if (check[amount]<INT_MAX){return check[amount];}
+        int ans = INT_MAX;
+        
+        for(int i=0;i<n;i++)
         {
-            if (vec[i] == -1){continue;}
-            for(long int j=0;j<n;j++)
-            {
-                if (i+coins[j]<=amount)
-                {
-                    if (vec[i+coins[j]] == -1){vec[i+coins[j]] = vec[i]+1;}
-                    else {vec[i+coins[j]] = min(vec[i+coins[j]],vec[i]+1);}
-
-                }
-                else
-                {
-                    break;
-                }
-            }
+            int res = func(coins,check,amount-coins[i]);
+            if (res==-1){continue;}
+            ans = min(ans,res);
         }
-        return vec[amount];
+        if (ans == -1){check[amount] = -1;return -1;}
+        if (ans == INT_MAX){ans = -1;check[amount] = ans;return ans;}
+        check[amount] = ans+1;
+        return ans+1;
+
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> check(amount+1, INT_MAX);
+        check[0] = 0;
+        n = coins.size();
+        int ans = func(coins,check,amount);
+        return ans;
     }
 };
