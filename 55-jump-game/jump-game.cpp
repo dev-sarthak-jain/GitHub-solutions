f60a0102ca1833c31c3d0d4ccc1267229c6ed0ca
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-18 02:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 51 MB (81.06%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -3,7 +3,6 @@ public:
     bool canJump(vector<int>& nums) {
         int maxIndex = 0;
         int n = nums.size();
-        if (maxIndex>=n-1){return true;}
         for(int i=0;i<n && i<=maxIndex;i++)
         {
             maxIndex = max(maxIndex,i+nums[i]);
