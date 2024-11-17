7bcf58fac5a49ed85fc8ef79c2f2cef971c579b8
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-18 01:17:18 +0530

    Time: 51 ms (7.44%) | Memory: 26.3 MB (7.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    string s1,s2;
     int n,m;
-    int recur(vector<vector<int>>& vec, int x,int y)
+    int func(vector<vector<int>>& vec, string& text1, string& text2, int x,int y)
     {
-        if (x==n || y==m){return 0;}
-        if (vec[x][y]!=-1){return vec[x][y];}
-        if (s1[x] == s2[y]){
-            vec[x][y] = recur(vec,x+1,y+1) + 1;
-            return vec[x][y];
-        }
-        else
-        {
-            vec[x][y] = max(recur(vec,x+1,y),recur(vec,x,y+1));
-            return vec[x][y];
-        }
+        if (x>=n || y>=m){return 0;}
+        else if (vec[x][y]!=-1){return vec[x][y];}
+        else if (text1[x] == text2[y]){vec[x][y] = 1 + func(vec,text1,text2,x+1,y+1);}
+        else {vec[x][y] = max(func(vec,text1,text2,x+1,y),func(vec,text1,text2,x,y+1));}
+        return vec[x][y];
     }
 
-    int longestCommonSubsequence(string t1, string t2) {
-        s1 = t1;
-        s2 = t2;
-        n = s1.length();
-        m = s2.length();
+    int longestCommonSubsequence(string text1, string text2) {
+        n = text1.length();
+        m = text2.length();
         vector<vector<int>> vec(n,vector<int>(m,-1));
-        return recur(vec,0,0);
+        return func(vec,text1,text2,0,0);
     }
 };
