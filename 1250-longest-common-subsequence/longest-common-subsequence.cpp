1e318fda82d0aadef4dc01bab29c7422420d71de
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-11-26 03:25:34 +0530

    Time: 23 ms (80.21%) | Memory: 26 MB (56.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,42 +2,21 @@ class Solution {
 public:
     int longestCommonSubsequence(string t1, string t2) {
         int n = t1.length(), m = t2.length();
-        vector<vector<int>> vec(n,vector<int>(m));
-        if (t1[0] == t2[0]){vec[0][0] = 1;}
-        for(int i=1;i<n;i++)
-        {
-            if (t1[i] == t2[0]){vec[i][0] = 1;}
-            else{vec[i][0] = vec[i-1][0];}
-        }
-        for(int i=1;i<m;i++)
-        {
-            if (t1[0] == t2[i]){vec[0][i] = 1;}
-            else{vec[0][i] = vec[0][i-1];}
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if (t1[i] == t2[j])
-                {
-                    vec[i][j] = vec[i-1][j-1] + 1;
-                }
-                else
-                {
-                    vec[i][j] = max(vec[i-1][j],vec[i][j-1]);
-                }
-            }
-        }
-        /*
+        vector<vector<int>> vec (n+1,vector<int>(m+1,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                cout << vec[i][j] << " ";
+                if (t1[i] == t2[j])
+                {
+                    vec[i+1][j+1] = vec[i][j] + 1;
+                }
+                else
+                {
+                    vec[i+1][j+1] = max(vec[i][j+1],vec[i+1][j]);
+                }
             }
-            cout << endl;
         }
-        */
-        return vec[n-1][m-1];
+        return vec[n][m];
     }
 };
