043d7fa471b301953761a691196f20d1202a01ab
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-12-01 17:51:03 +0530

    Time: 142 ms (5.01%) | Memory: 31.1 MB (8.05%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> check(n,vector<bool>(m,0));
+        int island = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if (grid[i][j]-'0' && !check[i][j])
+                {
+                    island++;
+                    cout << island << endl;
+                    stack<vector<int>> s;
+                    s.push({i,j});
+                    while(!s.empty())
+                    {
+                        vector<int> tuple = s.top();
+                        s.pop();
+                        int x = tuple[0], y = tuple[1];
+                        cout << x << " " << y << endl;
+                        check[x][y] = 1;
+                        if (x-1>=0 && grid[x-1][y]-'0' &&!check[x-1][y]){s.push({x-1,y});}
+                        if (y-1>=0 && grid[x][y-1]-'0' && !check[x][y-1]){s.push({x,y-1});}
+                        if (x+1<n && grid[x+1][y]-'0' && !check[x+1][y]){s.push({x+1,y});}
+                        if (y+1<m && grid[x][y+1]-'0' && !check[x][y+1]){s.push({x,y+1});}
+                    }
+                }
+            }
+        }
+        return island;
+    }
+};
