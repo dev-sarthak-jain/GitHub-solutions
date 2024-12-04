913a6b07ff13951a205fc8061df1af9a808c2b0c
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2024-12-05 03:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (16.40%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,5 +1,53 @@
 class Solution {
 public:
+    int n,m;
+    void BFS(vector<vector<char>>& board, int i, int j)
+    {
+        queue<pair<int,int>> s;
+        s.push({i,j});
+        vector<pair<int,int>> rec;
+        while(!s.empty())
+        {
+            pair<int,int> p = s.front();
+            s.pop();
+            int x = p.first, y = p.second;
+            if (x<0 || x>=n || y<0 || y>=m || board[x][y] == 'X' ||  board[x][y] == '#'){continue;}
+            board[x][y] = '#';
+            s.push({x+1,y});
+            s.push({x-1,y});
+            s.push({x,y+1});
+            s.push({x,y-1});
+        }
+    }
+
+    void solve(vector<vector<char>>& board) {
+        n = board.size();
+        m = board[0].size();
+        for (int i=0;i<n;i++)
+        {
+            if (board[i][0] == 'O'){BFS(board,i,0);}
+            if (board[i][m-1] == 'O'){BFS(board,i,m-1);}
+        }
+        for (int i=0;i<m;i++)
+        {
+            if (board[0][i] == 'O'){BFS(board,0,i);}
+            if (board[n-1][i] == 'O'){BFS(board,n-1,i);}
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if (board[i][j] == 'O'){board[i][j] = 'X';}
+                if (board[i][j] == '#'){board[i][j] = 'O';}
+            }
+        }
+    }
+};
+
+/*
+class Solution {
+public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
@@ -41,3 +89,4 @@ public:
         return ;
     }
 };
+*/
