46b3b495044ce47de18d1f10734e2f843127e11e
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2025-01-23 16:21:57 +0530

    Time: 689 ms (10.95%) | Memory: 276.1 MB (5.03%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<vector<int>>> memo;
+    int n,m;
+    vector<vector<vector<int>>> dp;
 
-    int func(vector<vector<int>>& coins, int x, int y, int neu) {
-        if (x >= n || y >= m) return INT_MIN;
-
-        if (x == n - 1 && y == m - 1) {
-            // Handle last cell: consider skipping it if `neu > 0`
-            return coins[x][y] < 0 && neu > 0 ? 0 : coins[x][y];
+    int func(vector<vector<int>>& coins, int x, int y, int k)
+    {
+        if (x>=n || y>=m){return INT_MIN;}
+        if (x==n-1 && y == m-1){
+            if (k>0 && coins[x][y]<0){return 0;}
+            return coins[x][y];
         }
+        if (dp[x][y][k]!=INT_MIN){return dp[x][y][k];}
 
-        if (memo[x][y][neu] != INT_MIN) return memo[x][y][neu];
+        int pathMax = INT_MIN;
 
-        int maxPath = INT_MIN;
-
-        // Normal moves
-        if (x + 1 < n) maxPath = max(maxPath, coins[x][y] + func(coins, x + 1, y, neu));
-        if (y + 1 < m) maxPath = max(maxPath, coins[x][y] + func(coins, x, y + 1, neu));
-
-        // Skip negative cell if `neu > 0`
-        if (neu > 0 && coins[x][y] < 0) {
-            if (x + 1 < n) maxPath = max(maxPath, func(coins, x + 1, y, neu - 1));
-            if (y + 1 < m) maxPath = max(maxPath, func(coins, x, y + 1, neu - 1));
+        if (x+1<n) pathMax = max(pathMax, coins[x][y] + func(coins,x+1,y,k));
+        if (y+1<m) pathMax = max(pathMax, coins[x][y] + func(coins,x,y+1,k));
+        if (coins[x][y]<0 && k >0)
+        {
+            if (x+1<n) pathMax = max(pathMax, func(coins,x+1,y,k-1));
+            if (y+1<m) pathMax = max(pathMax, func(coins,x,y+1,k-1));
         }
+        return dp[x][y][k] = pathMax;
 
-        return memo[x][y][neu] = maxPath;
     }
+
 
     int maximumAmount(vector<vector<int>>& coins) {
         n = coins.size();
         m = coins[0].size();
-        memo = vector<vector<vector<int>>>(n, vector<vector<int>>(m, vector<int>(3, INT_MIN)));
-        return func(coins, 0, 0, 2);
+        dp = vector<vector<vector<int>>>(n, vector<vector<int>>(m, vector<int>(3, INT_MIN)));
+        return func(coins,0,0,2);
     }
 };
