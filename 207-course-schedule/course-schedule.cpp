3d4da6ae952e31c10c7f9ab60e5e5570208d59e4
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2025-01-24 19:28:11 +0530

    Time: 55 ms (5.06%) | Memory: 19.1 MB (85.32%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> vec(n,0);
+        vector<vector<int>> graph = vector<vector<int>>(n);
+        for(int i=0;i<pre.size();i++)
+        {
+            graph[pre[i][0]].push_back(pre[i][1]);
+            vec[pre[i][1]]++;
+        }
+        int total = n;
+        while(total)
+        {
+            int i=0;
+            for(;i<n;i++)
+            {
+                if (vec[i] == 0){vec[i]--;total--;break;}
+            }
+            cout << i << " ";
+            if (i==n){return false;}
+            for(int j=0;j<graph[i].size();j++)
+            {
+                vec[graph[i][j]]--;
+                cout << "F " << graph[i][j];
+            }
+        }
+        return true;;
+
+    }
+};
