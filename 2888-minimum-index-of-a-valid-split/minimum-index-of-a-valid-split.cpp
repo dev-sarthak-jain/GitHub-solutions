661d192bb77b2f2c4c4bac92748c2fb28cfb70eb
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2025-03-27 13:43:51 +0530

    Time: 633 ms (5.33%) | Memory: 90 MB (70.90%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        int majority = nums[0];
+        int count = 1, total = 0;
+        for(int i=1;i<n;i++)
+        {
+            if (nums[i]!=majority && count == 0){majority = nums[i];count = 1;}
+            else if (nums[i] == majority){count++;}
+            else{count--;}
+        }
+        for(int i=0;i<n;i++)
+        {
+            if (majority==nums[i]){total++;}
+        }
+        count = 0;
+        for(float i=0;i<n-1;i++)
+        {
+            cout << count << endl;
+            if (nums[i] == majority){count++;}
+            if ((i+1)/2<count && (n-i-1)/2<(total-count)){return i;}
+        }
+        return -1;
+    }
+};
